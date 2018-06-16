cf42b6464f0384191db5c6c11b987d5cf99a62a9
Brenden Matthews <brenden@diddyinc.com>
2018-06-16 17:30:36 -0400

    Bump version.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="mother-of-dragons",
-    version="0.0.3",
+    version="0.0.4",
     author="Brenden Matthews",
     author_email="brenden@diddyinc.com",
     description="Python-based management tool for DragonMint T1",
